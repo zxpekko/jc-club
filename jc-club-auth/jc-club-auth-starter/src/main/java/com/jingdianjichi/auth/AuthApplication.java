5e1d0573010ad3c1052a9a39c3a806fd7d84e432
zxpekko <2250612787@qq.com>
2024-03-25 17:26:01 +0800

    分类模块开发

---
+++ jc-club-auth/jc-club-auth-starter/src/main/java/com/jingdianjichi/auth/AuthApplication.java
@@ -0,0 +1,22 @@
+package com.jingdianjichi.auth;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @Author:zxp
+ * @Description:
+ * @Date:11:09 2024/3/19
+ */
+@SpringBootApplication
+@MapperScan({"com.jingdianjichi.**.mapper","com.jingdianjichi.**.dao"})
+@ComponentScan("com.jingdianjichi.subject")
+
+public class AuthApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(AuthApplication.class,args);
+    }
+}
